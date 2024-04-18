1fbdd0cdb2071f554d44f55fdc3be2edf709df2a
sapnakanchar@77 <sapnakanchar771997@gmail.com>
2024-04-18 15:29:01 +0530

    sapna 1st commit

---
+++ Backend/SchoolMaster/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.response import Response
+import json
+from rest_framework.generics import GenericAPIView
+
+
+
+
+
+class AddSchool(GenericAPIView):
+    def post(self,request):
+       
+        return Response({"data":'',"response": {"n": 1, "msg": "Company added successfully","status": "success"}})
+           
