9ec843d25b6003a26898a7c3eb7039acbeb1f56c
sapnakanchar@77 <sapnakanchar771997@gmail.com>
2024-04-24 17:46:58 +0530

    sapna changes 24-04

--- Frontend/parent_student/views.py
+++ Frontend/parent_student/views.py
@@ -1,19 +1,40 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render, redirect, HttpResponse,HttpResponseRedirect
+import requests
 
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
-
+from school.static_info import frontend_url
 # Create your views here.
+class_list_url=frontend_url+'api/ClassMaster/List'
+parent_list_url = frontend_url+'api/Parent_StudentMaster/list'
 
 
 class parent_student_master(GenericAPIView):
     def get(self,request):
-        return render(request, 'admin/parent_student_master/parent_student_master.html',{})
+        tok = request.session.get('token', False)
+        if tok:
+            token = 'Bearer {}'.format(tok)
+            headers = {'Authorization':token}
+            parent_list_request = requests.get(parent_list_url,headers=headers)
+            parent_list_response = parent_list_request.json()
+            print("eeree",parent_list_response)
+            return render(request, 'admin/parent_student_master/parent_student_master.html',{'parentlist':parent_list_response['data']})
+        else:
+            return redirect('school:login')
+
+      
     
 class add_parent_student(GenericAPIView):
     def get(self,request):
-        return render(request, 'admin/parent_student_master/add_parent_student.html',{})
+        tok = request.session.get('token', False)
+        if tok:
+            token = 'Bearer {}'.format(tok)
+            headers = {'Authorization':token}
+            class_list_request = requests.get(class_list_url,headers=headers)
+            class_list_response = class_list_request.json()
+            return render(request, 'admin/parent_student_master/add_parent_student.html',{'classlist':class_list_response['data']})
+        else:
+            return redirect('school:login')
     
 class edit_parent_student(GenericAPIView):
     def get(self,request):
